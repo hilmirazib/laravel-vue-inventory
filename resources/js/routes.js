85c6e2f575c01a2d50c08cc17940765529668075
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2022-12-25 13:10:11 +0700

    templating admin and impelent jwt admin login

---
+++ resources/js/routes.js
@@ -0,0 +1,18 @@
+
+let login = require('./components/auth/login.vue').default;
+let register = require('./components/auth/register.vue').default;
+let forget = require('./components/auth/forget.vue').default;
+let dashboard = require('./components/auth/dashboard.vue').default;
+ 
+   
+  
+
+export const routes = [
+  { path: '/', component: login, name:'/'},
+  { path: '/register', component: register, name:'register'},
+  { path: '/forget', component: forget, name:'forget'},
+  { path: '/dashboard', component: dashboard, name:'dashboard'},
+
+
+
+]
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{vue_capture?}', function(){
+    return view('welcome');
+})->where('vue_capture', '[\/\w\.-]*');
